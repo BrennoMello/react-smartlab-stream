a347857361c4f970132da29ec1336b03b6531ee0
Brenno <brennodemello@hotmail.com>
2019-10-06 23:10:52 -0300

    Implementation Filter with Egent

--- NetBeansProjects/iot-reactive-application/src/main/java/controller/ReactiveController.java
+++ NetBeansProjects/iot-reactive-application/src/main/java/controller/ReactiveController.java
@@ -17,10 +17,14 @@ import io.vertx.mqtt.MqttClient;
 import io.vertx.mqtt.MqttClientOptions;
 import io.vertx.rxjava.core.eventbus.Message;
 import java.nio.file.Path;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import model.Device;
 import model.Sensor;
+import model.SensorData;
+import org.apache.edgent.topology.TStream;
 import org.apache.edgent.topology.Topology;
 import rx.Single;
 
@@ -57,6 +61,10 @@ public class ReactiveController extends AbstractVerticle {
             this.mqttOptions = new MqttClientOptions();
             this.mqttOptions.setLocalAddress("localhost");
             this.listDevices = new ArrayList<>();
+            
+            ControllerEdgent controlEdgent = new ControllerEdgent();
+            Topology topologyFilter = controlEdgent.createTopology();
+            
             jsonDevices = "[{id:ufbaino01, latitude:53.290411, longitude:-9.074406, sensors:[{id:temperatureSensor, type:Thermometer, collection_time:30000, publishing_time: 60000}, {id:humiditySensor, type:HumiditySensor, collection_time:30000, publishing_time: 60000}]},{id:ufbaino02, latitude:53.2865012, longitude:-9.0712183,sensors:[{id:temperatureSensor, type:Thermometer, collection_time:30000, publishing_time: 60000}, {id:currentSensor01, type:EnergyMeter, collection_time:1000, publishing_time: 60000}]}, {id:ufbaino03, latitude:53.2865015, longitude:-9.0712185,sensors:[{id:temperatureSensor, type:Thermometer, collection_time:30000, publishing_time: 60000}, {id:currentSensor01, type:HumiditySensor, collection_time:1000, publishing_time: 60000}]}]";
 
             System.out.println("subscribing in topics:");
@@ -75,21 +83,97 @@ public class ReactiveController extends AbstractVerticle {
                 }
 
             });
-         vertx.setPeriodic(2000, id  -> {
-        this.mqttClient.publishHandler(hndlr -> {
-              System.out.println("There are new message in topic: " + hndlr.topicName());
-              System.out.println("Content(as string) of the message: " + hndlr.payload().toString());
-              System.out.println("QoS: " + hndlr.qosLevel());
-              vertx.eventBus().send("webmedia",  hndlr.topicName() + hndlr.payload().toString() );
-             
-        }).subscribe("dev/ufbaino01/RES", 1);;
-         });
+            
+            String sensorData;
+            vertx.setPeriodic(2000, id  -> {
+            this.mqttClient.publishHandler(hndlr -> {
+                System.out.println("There are new message in topic: " + hndlr.topicName());
+                System.out.println("Content(as string) of the message: " + hndlr.payload().toString());
+                System.out.println("QoS: " + hndlr.qosLevel());
+                vertx.eventBus().send("webmedia",  hndlr.topicName() + hndlr.payload().toString() );
+                
+                TStream<String> temp = topologyFilter.strings(hndlr.payload().toString());
+                TStream<SensorData> tempSensorData = paserTatuStreamFlow(temp);
+                tempSensorData.sink(tuple -> System.out.println("Before Filter " + tuple.getValue()));
+                tempSensorData = tempSensorData.filter(tuple -> Double.parseDouble(tuple.getValue()) > 25 && Double.parseDouble(tuple.getValue()) < 37);
+                tempSensorData.sink(tuple -> System.out.println("After Filter " + tuple.getValue()));
+
+                controlEdgent.deployTopology(topologyFilter);
+               
+                System.out.println("Finish");
+                }).subscribe("dev/ufbaino01/RES", 1);;
+             });
+            
+            //filterData();
+            
         } catch (Exception e) {
             System.out.println("controller.ReactiveController.start()" + e.getMessage());
         }
+        
+        
+        
+        
     }
-    
    
+     private TStream<SensorData> paserTatuStreamFlow(TStream<String> tStream){
+      
+
+       TStream<SensorData> tStreamSensorData = tStream.map(tuple -> {
+                    List<SensorData> listData = new ArrayList<SensorData>();
+                    SensorData sensorData = null;
+                    try{
+                        
+                        if(TATUWrapper.isValidTATUAnswer(tuple)){
+                                
+                                
+                            JsonParser parser = new JsonParser();
+
+                            JsonElement element = parser.parse(tuple);
+                            JsonObject jObject = element.getAsJsonObject();
+
+
+                            JsonObject body = jObject.getAsJsonObject("BODY");
+                            
+                            JsonElement elementTimeStamp = body.get("TimeStamp");
+                            
+                            long delay = 0;
+                            if(elementTimeStamp != null){
+                             
+                                delay = System.currentTimeMillis()-elementTimeStamp.getAsLong();
+                                //System.out.println("Delay Message " + this.Sensorid + ": " + delay);
+                            
+                            }
+                            JsonArray jsonArray = body.getAsJsonArray("temperatureSensor");
+
+                            if(jsonArray != null){
+                                
+                                for (int i = 0; i < jsonArray.size(); i++) {
+                                    JsonElement jsonElement = jsonArray.get(i);
+                                    String value = String.valueOf(jsonElement.getAsDouble());
+                                    //System.out.println(value);
+                                    sensorData = new SensorData(value, LocalDateTime.now(), null, null, delay);  
+                                    //if(sensorData != null) 
+                                    //    listData.add(sensorData);
+                                }
+                                
+                            }
+                            
+                           
+                        }
+                        
+                    }catch(Exception e){
+                        System.out.println("Erro parser: " + e.getMessage());
+                    }
+                    
+                    return sensorData;
+		});
+      
+       return tStreamSensorData;
+   } 
+    
+   public void filterData(){
+       
+   }
 
     public void init() {
 
